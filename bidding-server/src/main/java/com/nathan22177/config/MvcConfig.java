a01a39c85be4185bf5f69974e6e32d099bcf1876
nathan22177 <nathanwillfox@gmail.com>
2019-10-31 17:40:30 +0300

    trying to fix spring mapping scripts in HTML

--- bidding-server/src/main/java/com/nathan22177/config/MvcConfig.java
+++ bidding-server/src/main/java/com/nathan22177/config/MvcConfig.java
@@ -3,6 +3,7 @@ package com.nathan22177.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,4 +14,10 @@ public class MvcConfig implements WebMvcConfigurer {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
 }
